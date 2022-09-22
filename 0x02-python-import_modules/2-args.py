eb9d5813871b9d8c8d5e3c78db6741cd56cbcc79
alekione <lexa9448@gmail.com>
2022-09-22 11:04:27 +0100

    modified:   2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,7 +5,10 @@ if __name__ == "__main__":
     if length == 0:
         print("0 arguments.")
     else:
-        print(f"{length} arguments:")
+        if length == 1:
+            print(f"{length} argument:")
+        else:
+            print(f"{length} arguments:")
         i = 0
         for item in sys.argv:
             if (i > 0):
