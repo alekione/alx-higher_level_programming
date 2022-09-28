2c6086bb3943979aa38b924953126d1eded8f788
alekione <lexa9448@gmail.com>
2022-09-28 14:10:02 +0100

    new file:   101-square_matrix_map.py 	new file:   102-complex_delete.py

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def complex_delete(a_dictionary, value):
+    to_del = []
+    if a_dictionary is None:
+        return None
+    for key, val in a_dictionary.items():
+        if val == value:
+            to_del.append(key)
+    for key in to_del:
+        del a_dictionary[key]
+    return a_dictionary
