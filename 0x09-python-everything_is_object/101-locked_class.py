09f21d03d7ebe59948ecb0706c1979c8e3575fa4
alekione <lexa9448@gmail.com>
2022-10-18 12:39:38 +0100

    new files

---
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+class LockedClass:
+    """ An example of a locked class that does not allow creation of
+    attributes other ths ' first_name'
+    """
+    __slots__ = "first_name"
