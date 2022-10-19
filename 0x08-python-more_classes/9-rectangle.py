c3974c549cc94d98f1a57dd8738e87ddc54e65f5
alekione <lexa9448@gmail.com>
2022-10-19 10:56:07 +0100

    modified:   9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -86,10 +86,6 @@ class Rectangle:
     @classmethod
     def square(cls, size=0):
         """ creates a rectangle object with all sides equal """
-        if type(size) != int:
-            raise TypeError("size must be an integer")
-        if size < 0:
-            raise ValueError("size must be >= 0")
         return Rectangle(size, size)
 
     @staticmethod
