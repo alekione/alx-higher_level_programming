05c74a6b52708f71ed7e3520e071f337a39b0cac
alekione <lexa9448@gmail.com>
2022-10-19 18:25:06 +0100

    modified:   101-locked_class.py 	modified:   104-line2.txt 	modified:   104-line3.txt 	modified:   105-line1.txt

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+""" A sample of a locked class """
+
+
 class LockedClass:
     """ An example of a locked class that does not allow creation of
     attributes other ths ' first_name'
