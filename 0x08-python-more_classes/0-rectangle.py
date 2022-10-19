9320fbda117cdafdcb7b2e40a79ab97ef50608b1
alekione <lexa9448@gmail.com>
2022-10-19 09:22:52 +0100

    new files

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+""" A class that is used to create a rectangle object """
+
+
+class Rectangle:
+    """ This is an ampty object """
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,2 @@
+This repo consists of files dealing with classes, objects, and data types in python
+Take what you can and all the best
