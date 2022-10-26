614d26528f255af5cd490c2eb572f2b3a4e8453a
alekione <lexa9448@gmail.com>
2022-10-26 21:45:04 +0100

    modified:   0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,5 @@
 def read_file(filename=''):
     """ read a file and print it to stdout """
     with open(filename, mode="r", encoding='UTF8') as fp:
-        print(fp.read())
+        for line in fp:
+            print (line)
